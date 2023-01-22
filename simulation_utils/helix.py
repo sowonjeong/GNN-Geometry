940cc1d0532869d0d381bfb17c8eba99d2244412
sowonjeong <97766158+sowonjeong@users.noreply.github.com>
2023-01-22 11:15:22 -0600

    add exp on circles and moons

--- simulation_utils/helix.py
+++ simulation_utils/helix.py
@@ -6,6 +6,10 @@ from sklearn.preprocessing import StandardScaler
 from torch_geometric.data import Data
 from torch_geometric.utils import from_scipy_sparse_matrix, to_undirected
 from sklearn.neighbors import kneighbors_graph
+import numbers
+from sklearn.utils import check_array, check_random_state
+from sklearn.utils import shuffle as util_shuffle
+from sklearn.utils.random import sample_without_replacement
 
 def generate_helix(alpha = 1, beta = 1, seed = 1234, n_samples = 3000, n_neighbours = 5, features = 'coordinates', standardize=True):
   random.seed(seed)
@@ -86,3 +90,162 @@ def get_vhelix(uniform=True, a = 0.5, b = 0.5, seed = 1234, n_samples = 3000, n_
         new_data = Data(x=torch.eye(n_samples), edge_index=edge_index,
                         edge_weight=edge_weights)
     return x,y, height, time, A, new_data
+
+
+def get_circles(n_samples=3000, *, shuffle=True, noise=None, random_state= 1234, factor=0.8,
+                n_neighbours = 5, features = 'coordinates', standardize=True):
+    """Make a large circle containing a smaller circle in 2d.
+    A simple toy dataset to visualize clustering and classification
+    algorithms.
+    Read more in the :ref:`User Guide <sample_generators>`.
+    Parameters
+    ----------
+    n_samples : int or tuple of shape (2,), dtype=int, default=100
+        If int, it is the total number of points generated.
+        For odd numbers, the inner circle will have one point more than the
+        outer circle.
+        If two-element tuple, number of points in outer circle and inner
+        circle.
+        .. versionchanged:: 0.23
+           Added two-element tuple.
+    shuffle : bool, default=True
+        Whether to shuffle the samples.
+    noise : float, default=None
+        Standard deviation of Gaussian noise added to the data.
+    random_state : int, RandomState instance or None, default=None
+        Determines random number generation for dataset shuffling and noise.
+        Pass an int for reproducible output across multiple function calls.
+        See :term:`Glossary <random_state>`.
+    factor : float, default=.8
+        Scale factor between inner and outer circle in the range `(0, 1)`.
+    Returns
+    -------
+    X : ndarray of shape (n_samples, 2)
+        The generated samples.
+    y : ndarray of shape (n_samples,)
+        The integer labels (0 or 1) for class membership of each sample.
+    """
+
+    if factor >= 1 or factor < 0:
+        raise ValueError("'factor' has to be between 0 and 1.")
+
+    if isinstance(n_samples, numbers.Integral):
+        n_samples_out = n_samples // 2
+        n_samples_in = n_samples - n_samples_out
+    else:
+        try:
+            n_samples_out, n_samples_in = n_samples
+        except ValueError as e:
+            raise ValueError(
+                "`n_samples` can be either an int or a two-element tuple."
+            ) from e
+
+    generator = check_random_state(random_state)
+    # so as not to have the first point = last point, we set endpoint=False
+    linspace_out = np.linspace(0, 2 * np.pi, n_samples_out, endpoint=False)
+    linspace_in = np.linspace(0, 2 * np.pi, n_samples_in, endpoint=False)
+    outer_circ_x = np.cos(linspace_out)
+    outer_circ_y = np.sin(linspace_out)
+    inner_circ_x = np.cos(linspace_in) * factor
+    inner_circ_y = np.sin(linspace_in) * factor
+
+    X = np.vstack(
+        [np.append(outer_circ_x, inner_circ_x), np.append(outer_circ_y, inner_circ_y)]
+    ).T
+    c = np.hstack(
+        [np.zeros(n_samples_out, dtype=np.intp), np.ones(n_samples_in, dtype=np.intp)]
+    )
+    if shuffle:
+        X, c = util_shuffle(X, c, random_state=generator)
+
+    if noise is not None:
+        X += generator.normal(scale=noise, size=X.shape)
+
+    A = kneighbors_graph(X, n_neighbours, mode='distance', include_self=False)
+    edge_index, edge_weights = from_scipy_sparse_matrix(A)
+    edge_index, edge_weights = to_undirected(edge_index, edge_weights)
+    if standardize:
+      preproc = StandardScaler()
+      X = preproc.fit_transform(X)
+    if features == 'coordinates':
+        new_data = Data(x=torch.from_numpy(X).float(),
+                        edge_index=edge_index,
+                        edge_weight=edge_weights)
+    else:
+        new_data = Data(x=torch.eye(n_samples), edge_index=edge_index,
+                        edge_weight=edge_weights)
+    return X,c, A, new_data
+
+def get_moons(n_samples=3000, *, shuffle=True, noise=None, random_state=1234,
+              n_neighbours = 5, features = 'coordinates', standardize=True):
+    """Make two interleaving half circles.
+    A simple toy dataset to visualize clustering and classification
+    algorithms. Read more in the :ref:`User Guide <sample_generators>`.
+    Parameters
+    ----------
+    n_samples : int or tuple of shape (2,), dtype=int, default=100
+        If int, the total number of points generated.
+        If two-element tuple, number of points in each of two moons.
+        .. versionchanged:: 0.23
+           Added two-element tuple.
+    shuffle : bool, default=True
+        Whether to shuffle the samples.
+    noise : float, default=None
+        Standard deviation of Gaussian noise added to the data.
+    random_state : int, RandomState instance or None, default=None
+        Determines random number generation for dataset shuffling and noise.
+        Pass an int for reproducible output across multiple function calls.
+        See :term:`Glossary <random_state>`.
+    Returns
+    -------
+    X : ndarray of shape (n_samples, 2)
+        The generated samples.
+    y : ndarray of shape (n_samples,)
+        The integer labels (0 or 1) for class membership of each sample.
+    """
+
+    if isinstance(n_samples, numbers.Integral):
+        n_samples_out = n_samples // 2
+        n_samples_in = n_samples - n_samples_out
+    else:
+        try:
+            n_samples_out, n_samples_in = n_samples
+        except ValueError as e:
+            raise ValueError(
+                "`n_samples` can be either an int or a two-element tuple."
+            ) from e
+
+    generator = check_random_state(random_state)
+
+    outer_circ_x = np.cos(np.linspace(0, np.pi, n_samples_out))
+    outer_circ_y = np.sin(np.linspace(0, np.pi, n_samples_out))
+    inner_circ_x = 1 - np.cos(np.linspace(0, np.pi, n_samples_in))
+    inner_circ_y = 1 - np.sin(np.linspace(0, np.pi, n_samples_in)) - 0.5
+
+    X = np.vstack(
+        [np.append(outer_circ_x, inner_circ_x), np.append(outer_circ_y, inner_circ_y)]
+    ).T
+    c = np.hstack(
+        [np.zeros(n_samples_out, dtype=np.intp), np.ones(n_samples_in, dtype=np.intp)]
+    )
+
+    if shuffle:
+        X, c = util_shuffle(X, c, random_state=generator)
+
+    if noise is not None:
+        X += generator.normal(scale=noise, size=X.shape)
+
+    A = kneighbors_graph(X, n_neighbours, mode='distance', include_self=False)
+    edge_index, edge_weights = from_scipy_sparse_matrix(A)
+    edge_index, edge_weights = to_undirected(edge_index, edge_weights)
+    if standardize:
+      preproc = StandardScaler()
+      X = preproc.fit_transform(X)
+    if features == 'coordinates':
+        new_data = Data(x=torch.from_numpy(X).float(),
+                        edge_index=edge_index,
+                        edge_weight=edge_weights)
+    else:
+        new_data = Data(x=torch.eye(n_samples), edge_index=edge_index,
+                        edge_weight=edge_weights)
+    return X,c, A, new_data
